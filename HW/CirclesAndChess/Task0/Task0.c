370ac8da765cf3e2f781b4bf0146fbed29642a4c
Maxim <90195390+MaximFLUNN@users.noreply.github.com>
2021-09-30 18:52:46 +0300

    Task0 update
    revised definition system

--- HW/CirclesAndChess/Task0/Task0.c
+++ HW/CirclesAndChess/Task0/Task0.c
@@ -6,43 +6,21 @@
 
 int main()
 {
-    double x1, y1, r1, x2, y2, r2, d, t, u1, u2;
-    t = 0.01;
+    double x1, y1, r1, x2, y2, r2, d, rmax, rmin;
+    //input
     printf("Input circle 1 x, y, r: \n");
     scanf("%f %f %f", &x1, &y1, &r1);
-    //printf("%f %f %f", x1, y1, r1);
     printf("Input circle 2 x, y, r: \n");
     scanf("%f %f %f", &x2, &y2, &r2);
+    //calculating distance
+    if (r1 > r2) { rmin = r2; rmax = r1; } else { rmin = r1; rmax = r2; }
     d = sqrt(pow((x1 - x2),2) + pow((y1 - y2),2));
-    if ((d > (r1 + r2)) && (r1 + d > r2) && (r2 + d > r1))
-        printf("Circles are separated\n");
-    else
-        if ((r1 + r2 == d) || (abs(r1 - r2) == d))
-            printf("Circles touch\n");
-        else
-            printf("Circles not touch");
+    //calculating the cause
+	if (d == (r1 + r2)) {printf("The circles touch\n");}
+	if ((d < (r1 + r2)) && (d > rmax)) {printf("The circles intersect\n");}
+	if (d > (r1 + r2) && (d > rmax)) {printf("Circles don't touch\n");}
+	if ((d + rmin) < rmax) {printf("Circle inside other\n");}
     system("PAUSE");
-
     return 0;
 }
-/*
-if (d > (r1 + r2))
-    {
-        printf("Circles are separated\n");
-    }
-    if (fabs(d - (r1 + r2)) < t)
-    {
-        printf("Circles touch\n");
-    }
-    if (d < r1 + r2)
-    {
-        if (d > r1 - r2)
-        {
-            printf("Circles intersect\n");
-        }
-        else
-        {
-            printf("Circles inside\n");
-        }
-    }
-*/
+//Powered MaximFLUNN (Maxim Filatov 3821b1pr2)
