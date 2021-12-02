a57aa071c0b2bfaf426fa3f8a0fb6adfeb828c77
Maxim Filatov :3 <90195390+MaximFLUNN@users.noreply.github.com>
2021-12-02 17:41:08 +0300

    Divided all tasks into folders

---
+++ TasksInTerminalClass/TaskTerminalClass/InverseNumbers/Source code - InverseNumbers.c
@@ -0,0 +1,46 @@
+﻿/*
+                | Copyright 2021 | MaximFLUNN | Maxim Filatov Group: 3821B1PR2 |
+                |      GitHub profile link: https://github.com/MaximFLUNN      |
+*/
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main() {
+	int number, isWork = 1, choose;
+	printf("Mode one - inverse numbers\n");
+	printf("Mode two - sum numbers\n");
+	printf("Choose mode (1 or 2): ");
+	scanf("%d", &choose);
+	if (choose > 2 || choose < 1) {
+		printf("\nError mode number!\n");
+		return 0;
+	}
+	while (isWork) {
+		printf("Let`s input numbers!\n");
+		printf("For exit enter 0\n");
+		int isError = 0;
+		do {
+			if (isError) printf("Wrong input! Try again.");
+
+			printf("\nInput positive number: ");
+			scanf("%d", &number);
+			if (number == 0) isWork = 0; break;
+
+			isError = (number <= 0);
+
+		} while (isError);
+		int inverse = 0, sum = 0, numberclone = number;
+		while (numberclone) {
+			sum += numberclone % 10;
+			numberclone = numberclone / 10;
+		}
+		while (number) {
+			inverse *= 10;
+			inverse += number % 10;
+			number = number / 10;
+		}
+		if (isWork == 1 & choose == 1) printf("\nResult: %d\n\n", inverse);
+		if (isWork == 1 & choose == 2) printf("\nResult: %d\n\n", sum);
+		if (!isWork) printf("\n\nEXIT\n\n");
+	}
+	return 0;
+}
---
+++ TasksInTerminalClass/TaskTerminalClass/Verification work (15.10.2021) Task 1/Source code Verification work (15.10.2021) Task 1.c
@@ -0,0 +1,37 @@
+﻿/*
+                | Copyright 2021 | MaximFLUNN | Maxim Filatov Group: 3821B1PR2 |
+                |      GitHub profile link: https://github.com/MaximFLUNN      |
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <time.h>
+#include <stdio.h>
+int main() {
+	int n, x, A, number, i = 0, sdvig, k;
+	srand(time(NULL));
+	printf("\nInput number(N): ");
+	scanf("%d", &n);
+	printf("\nInput number(-A): ");
+	scanf("%d", &x);
+	printf("\nInput number(A): ");
+	scanf("%d", &A);
+
+	int* MainArr = (int*)malloc(n * sizeof(int));
+	for (i = 0; i < n; i++) {
+		MainArr[i] = number = rand() % (A + x + 1) - x;
+		printf("%d ", MainArr[i]);
+	}
+	printf("\nInput number(k): ");
+	scanf("%d", &k);
+	for (int g = 0; g < k; g++) {
+		sdvig = MainArr[n - 1];
+		for (i = n - 1; i > 0; i--) {
+			MainArr[i] = MainArr[i - 1];
+		}
+		MainArr[0] = sdvig;
+	}
+	printf("\n Before:\n");
+	for (i = 0; i < n; i++)
+		printf("%d ", MainArr[i]);
+	return 0;
+}
