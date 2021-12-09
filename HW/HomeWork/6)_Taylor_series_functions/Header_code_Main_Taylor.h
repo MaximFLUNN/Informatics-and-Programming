4945b4cb1ee18d3d00931dafcb5639011b4abab5
Maxim Filatov :3 <90195390+MaximFLUNN@users.noreply.github.com>
2021-12-09 18:24:28 +0300

    Update 2.1 Task 6
    Delete folder debug

---
+++ HW/HomeWork/6)_Taylor_series_functions/Header_code_Main_Taylor.h
@@ -0,0 +1,38 @@
+﻿/*
+                | Copyright 2021 | MaximFLUNN | Maxim Filatov Group: 3821B1PR2 |
+                |      GitHub profile link: https://github.com/MaximFLUNN      |
+-------------------------------------------------------------------------------------------------------
+                                       | Header main file |
+*/
+#ifndef _MAIN_H
+#define _MAIN_H
+
+#include "Header_code_Func_Taylor.h"
+#include "Header_code_localization.h"
+
+#define PI 3.1415926535897931
+
+// [ENG] Variables | [RU] Переменные |
+float eps;
+int N, SelectFunc, SelectMode, i, language, mode, settings, Nmax, Number_experiment;;
+double x, xtmp, X_clone, Best_result_error;;
+
+
+
+// [ENG] Functions (int) | [RU] Функции (int) |
+int Select_language();
+int Select_mode();
+int Select_func();
+int Select_settings();
+int Get_Value_N();
+int Get_Value_Nmax();
+
+// [ENG] Functions (double) | [RU] Функции (double) |
+double Get_Value_X();
+double Get_Value_Eps();
+double Best_experiment(int language, int Number_experiment, double Best_result_error);
+
+// [ENG] Function (void) | [RU] Функция (void) |
+void restart();
+
+#endif 
